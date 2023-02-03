a0d787f18fb0e8e66d071599c7d0b95ff503d354
Dmytro <mishamakar5@gmail.com>
2023-02-03 15:57:54 +0200

    add components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import Dashboard from './components/Dashboard/Dashboard';
+import Preferences from './components/Preferences/Preferences';
+
+import {
+  BrowserRouter,
+  Route,
+  Switch
+} from "react-router-dom";
+
 import './App.css';
 
 function App() {
   return (
-    <div>
-      
+    <div className="wrapper">
+      <h1>Application</h1>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+          <Route path="/preferences">
+            <Preferences />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
